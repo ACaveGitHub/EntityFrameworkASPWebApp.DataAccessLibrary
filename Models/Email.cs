83cb885e3d3906b2e3fe64512de2ae74c2f87530
Adam Cave <adacave_09@hotmail.com>
2021-03-15 18:37:21 -0500

    Add project files.

---
+++ Models/Email.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DataAccess.Models
+{
+   public class Email
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(200)]        
+        public String EmailAddress { get; set; }
+    }
+}
